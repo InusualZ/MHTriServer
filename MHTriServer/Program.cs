79b671b21497df9ca0ccc53a342d9aaa1abc5397
InusualZ <wesmoret98@gmail.com>
2021-03-13 01:32:00 -0400

    Remove PcapngUtils
    Left over from when I was extracting the packets from the network dump

--- MHTriServer/Program.cs
+++ MHTriServer/Program.cs
@@ -1,52 +1,14 @@
 ﻿using MHTriServer.Player;
 using MHTriServer.Server;
-using MHTriServer.Server.Packets;
-using PcapngUtils.Common;
-using PcapngUtils.PcapNG;
 using System;
-using System.IO;
-using System.Threading;
 
 namespace MHTriServer
 {
     class Program
     {
 
-        public static void OpenPcapFile(string filename, CancellationToken token)
-        {
-            using (var reader = new PcapNGReader(filename, false))
-            {
-                reader.OnReadPacketEvent += reader_OnReadPacketEvent;
-                reader.ReadPackets(token);
-                reader.OnReadPacketEvent -= reader_OnReadPacketEvent;
-            }
-        }
-
-        static void reader_OnReadPacketEvent(object context, IPacket packet)
-        {
-            using var packetStream =  new MemoryStream(packet.Data[54..]);
-            var packetReader = new ExtendedBinaryReader(packetStream, Endianness.Big);
-            var size = packetReader.ReadUInt16();
-            var counter = packetReader.ReadUInt16();
-            var packetId = packetReader.ReadUInt32();
-            var packetData = Packet.CreateFrom(packetId, size, counter);
-            if (packetData != null)
-            {
-                Console.WriteLine($"Packet {packetData.GetType().Name} {packetData.ID:X8}");
-            }
-            else
-            {
-                Console.WriteLine($"Unknown Packet {packetId:X8}");
-            }
-        }
-
-
         public static int Main(string[] args)
         {
-            var token = new CancellationToken();
-            OpenPcapFile(@"D:\Monster Hunter Tri\ngevan2k\selected.pcapng", token);
-            return 0;
-
             var playerManager = new PlayerManager();
 
             var opnServer = new OPNServer(playerManager);
