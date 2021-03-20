f645a8a8dc611ef964037e6d6bd7bafaba8d535f
InusualZ <wesmoret98@gmail.com>
2021-03-20 08:50:12 -0400

    Fix field name and reading assert

--- MHTriServer/Server/Packets/AnsTerms.cs
+++ MHTriServer/Server/Packets/AnsTerms.cs
@@ -6,20 +6,20 @@ namespace MHTriServer.Server.Packets
     {
         public const uint PACKET_ID = 0x62110200;
 
-        public uint TermsLength { get; private set; }
+        public uint Offset { get; private set; }
 
         public uint UnknownField { get; private set; }
 
         public string TermsMessage { get; private set; }
 
-        public AnsTerms(uint termsLength, uint unknownField, string termsMessage) : base(PACKET_ID) => (TermsLength, UnknownField, TermsMessage) = (termsLength, unknownField, termsMessage);
+        public AnsTerms(uint termsLength, uint unknownField, string termsMessage) : base(PACKET_ID) => (Offset, UnknownField, TermsMessage) = (termsLength, unknownField, termsMessage);
 
         public AnsTerms(uint id, ushort size, ushort counter) : base(id, size, counter) { }
 
         public override void Serialize(ExtendedBinaryWriter writer)
         {
             base.Serialize(writer);
-            writer.Write(TermsLength);
+            writer.Write(Offset);
             writer.Write(UnknownField);
             writer.Write(TermsMessage);
         }
@@ -27,15 +27,15 @@ namespace MHTriServer.Server.Packets
         public override void Deserialize(ExtendedBinaryReader reader)
         {
             Debug.Assert(ID == PACKET_ID);
-            Debug.Assert(Size == 8);
-            TermsLength = reader.ReadUInt32();
+            Debug.Assert(Size == 12);
+            Offset = reader.ReadUInt32();
             UnknownField = reader.ReadUInt32();
             TermsMessage = reader.ReadString();
         }
 
         public override string ToString()
         {
-            return base.ToString() + $":\n\tTermsLength {TermsLength}\n\tUnknownField {UnknownField}\n\tTermsMessage {TermsMessage}";
+            return base.ToString() + $":\n\tOffset {Offset}\n\tUnknownField {UnknownField}\n\tTermsMessage {TermsMessage}";
         }
     }
 }
