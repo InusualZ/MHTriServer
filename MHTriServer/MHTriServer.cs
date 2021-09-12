5b05cc7a7c20c1085306ae8dca209f3266476b70
InusualZ <wesmoret98@gmail.com>
2021-09-12 19:22:14 -0400

    Start using a logging service (Using `log4net`)

--- MHTriServer/MHTriServer.cs
+++ MHTriServer/MHTriServer.cs
@@ -1,14 +1,38 @@
-﻿using MHTriServer.Player;
+﻿using log4net;
+using log4net.Config;
+using MHTriServer.Player;
 using MHTriServer.Server;
 using System;
+using System.IO;
+using System.Reflection;
+using System.Threading;
 
 namespace MHTriServer
 {
     public class MHTriServer
     {
+        public static readonly ILog Log = LogManager.GetLogger(nameof(MHTriServer));
+
+        public static void InitializeLogger()
+        {
+            var executingAssembly = Assembly.GetExecutingAssembly();
+            var filepath = Path.Join(Path.GetDirectoryName(executingAssembly.Location), "log4net.xml");
+            if (!File.Exists(filepath))
+            {
+                throw new ApplicationException("Unable to initialize the logging service\nPlease verify that `log4net.xml` exist at the root folder of the program binary");
+            }
+
+            var repo = LogManager.GetRepository(executingAssembly);
+            XmlConfigurator.Configure(repo, new FileInfo(filepath));
+
+            // Setup Thread Name
+            Thread.CurrentThread.Name = nameof(MHTriServer);
+        }
 
         public static int Main(string[] args)
         {
+            InitializeLogger();
+
             var playerManager = new PlayerManager();
 
             var opnServer = new OPNServer(playerManager);
